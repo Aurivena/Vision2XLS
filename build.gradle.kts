0a85006aa4277dbc8e09f39dc2d3d477a6e10d04
Aurivena <vladkonovalov20060@gmail.com>
2026-01-14 18:25:32 +0500

    uodate build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("application")
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.aurivena.lms"
@@ -13,7 +14,7 @@ repositories {
 
 javafx {
     version = "20.0.2"
-    modules("javafx.controls")
+    modules("javafx.controls", "javafx.fxml", "javafx.graphics")
 }
 
 application {
@@ -26,9 +27,18 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("org.apache.poi:poi-ooxml:5.5.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("Vision2XLS")
+    archiveClassifier.set("")
+    manifest {
+        attributes["Main-Class"] = "app.view.SimpleUi"
+    }
 }
